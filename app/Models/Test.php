cc8b0b8714b01469c1b245f406da46f9158fd87d
todd <hey_todd@foxmail.com>
2017-10-31 09:59:28 +0000

    email

---
+++ app/Models/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+    //
+    protected $table = "test";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password'
+    ];
+}
